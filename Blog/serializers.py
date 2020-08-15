f618441aba2f5cc6934baad4bfb072bb3aaca6e4
Sajid16 <sajid.mahboob16@gmail.com>
2020-08-15 22:52:22 +0600

    replacing categoryID with category name in serializer.py

--- Blog/serializers.py
+++ Blog/serializers.py
@@ -8,11 +8,11 @@ class categorySerializer(serializers.ModelSerializer):
 
 
 class articlesSerializer(serializers.ModelSerializer):
-    # category_name = serializers.SerializerMethodField(source='get_category_name')
+    category_name = serializers.SerializerMethodField(source='get_category_name')
     # category_name = serializers.CharField(source='category.name')
     class Meta:
         model = Articles
-        fields = ['id','name', 'description', 'category', 'created_at', 'updated_at']
+        fields = ['id','name', 'description', 'category_name', 'created_at', 'updated_at']
 
     # getting field name from foreign key instead of id
     def get_category_name(self, obj):
