ef4585f3cb4874f4e15156900a97bd457a296e49
Sajid16 <sajid.mahboob16@gmail.com>
2020-08-22 01:35:04 +0600

    change done in views.py JsonResponse of Tutorial app

--- Tutorial/views.py
+++ Tutorial/views.py
@@ -4,5 +4,6 @@ from django.http import JsonResponse, HttpResponse
 # Create your views here.
 
 def test(request):
-    return JsonResponse("it is working properly")
+    message = "it is working properly"
+    return JsonResponse(message)
     # return HttpResponse("it is working properly")
